5290dee0303392e247a47c7b481341235f10b5ff
Doridian <git@doridian.net>
2022-07-13 15:14:53 -0700

    And make it actually compile

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 cd "$(dirname "$0")"
 
 rm -rf output && mkdir -p output
+rm -rf input/tmp && mkdir -p input/tmp
 
 export DEFAULT_KERNEL_MODULES="8021q af_packet bridge dwc2 garp i2c-mux i2c-mux-pinctrl ipv6 llc pps-gpio pps-ldisc raspberrypi-hwmon roles rtc-pcf85063 stp"
 export CMDLINE="console=tty1 root=/dev/root rootfstype=ext4 fsck.repair=yes ro rootwait"
@@ -11,8 +12,8 @@ export CMDLINE="console=tty1 root=/dev/root rootfstype=ext4 fsck.repair=yes ro r
 git rev-parse HEAD > input/rootfs/etc/image_commit
 date > input/rootfs/etc/image_date
 
-docker build -t ntp-alpine-compiler compiler
-docker run --rm -it --entrypoint=/input/compile.sh -v "$PWD/input:/input" -v "$PWD/output:/output" ntp-alpine-compiler
+#docker build -t ntp-alpine-compiler compiler
+docker run --rm -it --entrypoint=/input/compile.sh -v "$PWD/input:/input" ntp-alpine-compiler
 
 IMG="ghcr.io/raspi-alpine/builder"
 docker pull "$IMG"
--- input/compile.sh
+++ input/compile.sh
@@ -5,22 +5,17 @@ cd /tmp
 git clone https://github.com/wowczarek/ptpd.git
 cd ptpd
 
-autoreconf -vi
-
-./configure
-
-
 DEF_FILE='src/constants.h'
 echo '' >> "$DEF_FILE"
 echo 'typedef unsigned char u_char;' >> "$DEF_FILE"
 echo 'typedef unsigned int u_int;' >> "$DEF_FILE"
 echo 'typedef unsigned short u_short;' >> "$DEF_FILE"
 
-CONFIG_H='src/config.h'
-echo '' >> "$CONFIG_H"
-echo '#undef HAVE_NETINET_IF_ETHER_H' >> "$CONFIG_H"
+find src/libcck -type f | xargs -n1 sed 's~#include <linux/ethtool.h>~#include <netinet/if_ether.h>\n#include <linux/ethtool.h>~' -i
+find src/libcck -type f | xargs -n1 sed 's~#include <linux/if_ether.h>~#include <netinet/if_ether.h>\n#include <linux/if_ether.h>~' -i
 
-#cat config.h
-#exit 1
-
+autoreconf -vi
+./configure
 make "-j$(nproc)"
+
+cp src/ptpd /input/rootfs/sbin/ptpd
