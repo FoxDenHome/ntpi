50b33fc2d2302e85df1e5d149b02e9f1f01997e6
Doridian <git@doridian.net>
2022-07-14 00:04:21 -0700

    rm ptpd

--- input/compile.sh
+++ input/compile.sh
@@ -4,27 +4,27 @@ set -e
 
 
 
-echo "#######################################"
-echo "COMPILING PTPD"
-echo "#######################################"
-
-cd /tmp
-git clone https://github.com/wowczarek/ptpd.git
-cd /tmp/ptpd
-
-DEF_FILE='src/constants.h'
-echo '' >> "$DEF_FILE"
-echo 'typedef unsigned char u_char;' >> "$DEF_FILE"
-echo 'typedef unsigned int u_int;' >> "$DEF_FILE"
-echo 'typedef unsigned short u_short;' >> "$DEF_FILE"
-
-find src/libcck -type f | xargs -n1 sed 's~#include <linux/ethtool.h>~#include <netinet/if_ether.h>\n#include <linux/ethtool.h>~' -i
-find src/libcck -type f | xargs -n1 sed 's~#include <linux/if_ether.h>~#include <netinet/if_ether.h>\n#include <linux/if_ether.h>~' -i
-
-autoreconf -vi
-./configure
-make "-j$(nproc)"
-cp src/ptpd /input/rootfs/sbin/
+#echo "#######################################"
+#echo "COMPILING PTPD"
+#echo "#######################################"
+#
+#cd /tmp
+#git clone https://github.com/wowczarek/ptpd.git
+#cd /tmp/ptpd
+#
+#DEF_FILE='src/constants.h'
+#echo '' >> "$DEF_FILE"
+#echo 'typedef unsigned char u_char;' >> "$DEF_FILE"
+#echo 'typedef unsigned int u_int;' >> "$DEF_FILE"
+#echo 'typedef unsigned short u_short;' >> "$DEF_FILE"
+#
+#find src/libcck -type f | xargs -n1 sed 's~#include <linux/ethtool.h>~#include <netinet/if_ether.h>\n#include <linux/ethtool.h>~' -i
+#find src/libcck -type f | xargs -n1 sed 's~#include <linux/if_ether.h>~#include <netinet/if_ether.h>\n#include <linux/if_ether.h>~' -i
+#
+#autoreconf -vi
+#./configure
+#make "-j$(nproc)"
+#cp src/ptpd /input/rootfs/sbin/
 
 
 
