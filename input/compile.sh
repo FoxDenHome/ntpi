503a783e172be4383feadd94f71f7e7240e36dac
Doridian <git@doridian.net>
2022-07-14 20:33:10 -0700

    start adding nmea basics

--- input/compile.sh
+++ input/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 
 
@@ -34,8 +34,10 @@ echo "#######################################"
 echo "COMPILING LINUXPTP"
 echo "#######################################"
 mkdir -p /tmp/ptp
-tar -C/tmp/ptp -xvf /input/src/linuxptp-3.1.1.tgz
+tar -C/tmp/ptp -xf /input/src/linuxptp-3.1.1.tgz
 cd /tmp/ptp/linuxptp-3.1.1
+
+sed 's~#define BAUD\s.*~#define BAUD 115200~g' -i ts2phc_nmea_master.c
 
 make "-j$(nproc)"
 make install
