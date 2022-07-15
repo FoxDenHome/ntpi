17bd5b609f9e954cf7cf9f641c8a22f5c744448d
Doridian <git@doridian.net>
2022-07-15 14:15:02 -0700

    Provide info if the last update was caused by lstab update

--- input/image.sh
+++ input/image.sh
@@ -9,7 +9,7 @@ echo 'include usercfg.txt' >> "$BOOTFS_PATH/config.txt"
 # Install packages
 chroot_exec apk update
 chroot_exec apk upgrade
-chroot_exec apk add bridge-utils curl screen prometheus-node-exporter gpsd gpsd-clients chrony bridge wget sudo tcpdump nano openssh-sftp-server ethtool keepalived keepalived-openrc python3 py3-pyserial py3-gpsd raspberrypi libc6-compat
+chroot_exec apk add bridge-utils curl screen prometheus-node-exporter gpsd gpsd-clients chrony bridge wget sudo tcpdump nano openssh-sftp-server ethtool keepalived keepalived-openrc python3 py3-pyserial py3-gpsd py3-requests raspberrypi libc6-compat
 
 # Configure services
 chroot_exec rc-update del ntpd default
