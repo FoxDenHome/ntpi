f2987c22fe39a62520f4ed41395fa50d76a2f42e
Doridian <git@doridian.net>
2022-07-16 12:47:33 -0700

    rm ptpd, we wont need that

--- input/compile.sh
+++ input/compile.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 set -ex
 
-
-
-
-#echo "#######################################"
-#echo "COMPILING PTPD"
-#echo "#######################################"
-#
-#cd /tmp
-#git clone https://github.com/wowczarek/ptpd.git
-#cd /tmp/ptpd
-#
-#DEF_FILE='src/constants.h'
-#echo '' >> "$DEF_FILE"
-#echo 'typedef unsigned char u_char;' >> "$DEF_FILE"
-#echo 'typedef unsigned int u_int;' >> "$DEF_FILE"
-#echo 'typedef unsigned short u_short;' >> "$DEF_FILE"
-#
-#find src/libcck -type f | xargs -n1 sed 's~#include <linux/ethtool.h>~#include <netinet/if_ether.h>\n#include <linux/ethtool.h>~' -i
-#find src/libcck -type f | xargs -n1 sed 's~#include <linux/if_ether.h>~#include <netinet/if_ether.h>\n#include <linux/if_ether.h>~' -i
-#
-#autoreconf -vi
-#./configure
-#make "-j$(nproc)"
-#cp src/ptpd /input/rootfs/sbin/
-
-
-
-
 LINUXPTP_VERSION=3.1
 LINUXPTP_VERSION_FULL=3.1.1
 echo "#######################################"
