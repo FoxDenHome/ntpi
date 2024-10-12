e5a06900329f5e384465b4f02b822c3ece28a592
Doridian <git@doridian.net>
2024-10-11 19:04:17 -0700

    n

--- upgrade.sh
+++ upgrade.sh
@@ -8,4 +8,4 @@ put output/sdcard_update.img.gz /tmp/sdcard_update.img.gz
 put output/sdcard_update.img.gz.sha256 /tmp/sdcard_update.img.gz.sha256
 EOF
 
-ssh -t "$DEST" 'sudo -n /bin/sh -l -c "ab_flash /tmp/sdcard_update.img.gz && reboot"'
+ssh -t "$DEST" 'sudo /bin/sh -l -c "ab_flash /tmp/sdcard_update.img.gz && reboot"'
