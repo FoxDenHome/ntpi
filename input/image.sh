e8147abfbe0a2450e3ea0683fa56d969945d7324
Doridian <git@doridian.net>
2025-09-02 15:43:20 -0700

    Add dbus and polkit

--- input/image.sh
+++ input/image.sh
@@ -56,7 +56,9 @@ chroot_exec apk add \
                 sudo-ldap@testing \
                 tcpdump \
                 wget \
-                ncdu
+                ncdu \
+                dbus \
+                polkit
 
 # Configure services
 chroot_exec rc-update del rngd sysinit
@@ -69,6 +71,8 @@ chroot_exec rc-update add kanidm-unixd default
 chroot_exec rc-update add kanidm-unixd-tasks default
 chroot_exec rc-update add nscd default
 chroot_exec rc-update add hwclock
+chroot_exec rc-update add dbus
+chroot_exec rc-update add polkit
 
 # Configure kernel modules
 LOAD_KERNEL_MODULES='8021q af_packet bridge dwc2 garp i2c-dev i2c-mux i2c-mux-pinctrl ipv6 llc pps-gpio pps-ldisc raspberrypi-hwmon roles rtc-pcf85063 stp ftdi_sio'
